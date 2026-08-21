a3e043f93c6294bfdb013d87b88d8ffa1ca948ff
estadiosvirtuales <estadiosvirtuales1@gmail.com>
2026-08-21 12:17:08 -0300

    23

--- juego.js
+++ juego.js
@@ -5478,7 +5478,7 @@ async function compartirCartaFUT() {
             </div>
             <div class="poster-badge">
                 <img class="poster-badge-icon icon-poster-racha" src="fuego.png" alt="Racha">
-                <span>${racha} días de racha</span>
+                <span>${racha} Días de racha</span>
             </div>
             <div class="poster-badge">
                 <img class="poster-badge-icon icon-poster-victorias" src="trofeo.png" alt="Victorias">
